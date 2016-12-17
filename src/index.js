40bdf800d3810f6221fe595d9355f53c68beb334
Michael Braun <michaelsoftware1997@gmail.com>
2016-12-17 17:03:11 +0100

    Added polyfill for promises

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import LoginHelper from './main/login/login';
 import './jui/juiLoader';
 import './main/loading/loading';
 import './main/overlay/overlay';
+import Promise from 'promise-polyfill';
+
 
 
 window.ready(function () {
